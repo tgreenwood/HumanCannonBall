17cc5d199292a621c865273cbec1c298efd21321
Anton <anton.tgw@gmail.com>
2015-06-26 00:08:33 +0300

    Created project

---
+++ HumanCannonBall-android/src/com/tgreenwood/humancannonball/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.tgreenwood.humancannonball.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.tgreenwood.humancannonball.HCBGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new HCBGame(), config);
+	}
+}
---
+++ HumanCannonBall/html/src/com/tgreenwood/humancannonball/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.tgreenwood.humancannonball.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.tgreenwood.humancannonball.HCBGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new HCBGame();
+        }
+}
