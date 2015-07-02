30bbef10dc45b5edc7771b2a2c4401bc102ea98e
Anton Pikhtin <anton.tgw@gmail.com>
2015-07-02 10:57:52 +0300

    complete project rebuild

---
+++ android/gen/com/tgreenwood/humancannonball/android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.tgreenwood.humancannonball.android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
